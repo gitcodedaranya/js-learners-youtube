4dc2048216fa43dfefe85bbdcb1ec08a2c8c9c5f
WebdevAD <aranya.webdeveloper@gmail.com>
2025-07-21 15:27:35 +0000

    datatypes

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,12 @@
+let name = 'Asohk'; //string datatype
+let age= '12'; //number datatype
+let IsLoggedIn = false //boolean datatype
+
+// number
+// bigint
+// boolean => true/false
+// null => Standalone value
+// undefined =>
+// symbol => unique
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined

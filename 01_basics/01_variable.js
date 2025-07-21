6b84a1cff5c13ef87877ce7d6fdaab3443cfb215
WebdevAD <aranya.webdeveloper@gmail.com>
2025-07-21 15:31:25 +0000

    datatypes

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,6 +1,13 @@
 const accountId = 1443;
 let accountEmail = 'test@test.com';
-var password = '123';
+var accountPassword = '123';
 accountCity = 'Jaipur';
-accountId = 2;
+//accountId = 2;
+accountEmail = 'testchange@gmail.com'
+accountPassword = 'hh';
+accountCity = 'Udaypur' // also be assigned like this but don't use it
+let accountState ; 
 console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+/* Prefer not to use var
+because of issue in block scope & functional scope, Only use const & let */

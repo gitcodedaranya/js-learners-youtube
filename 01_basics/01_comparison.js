976781dca482b993a4b2463f52d17cd7f390738b
WebdevAD <aranya.webdeveloper@gmail.com>
2025-07-21 18:10:36 +0000

    comparison

--- 01_basics/01_comparison.js
+++ 01_basics/01_comparison.js
@@ -1,1 +1,14 @@
-console.log(2 > 1);
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+//reason is Comparions convet null to a number, treating it as 0
+
+// console.log(undefined > 0); //false
+// console.log(undefined == 0); //false
+// console.log(undefined >= 0); //false
+
+console.log("2" === 2) //false

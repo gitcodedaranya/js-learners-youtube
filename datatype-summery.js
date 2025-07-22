2add0c7b5959bb1c31a721d380ac5422be1686f4
WebdevAD <aranya.webdeveloper@gmail.com>
2025-07-22 15:09:06 +0000

    datatypesummery

---
+++ datatype-summery.js
@@ -0,0 +1,10 @@
+// There are two types of data types based on How data is stored in memory and how it is accessed
+
+// Primitive
+// 7 Types - String, Number, Boolean, null, undefined, Symbol, Bigint
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+console.log(id === anotherId)
+//Non primitive or reference Type
+// Array, Objects, Functions, 
